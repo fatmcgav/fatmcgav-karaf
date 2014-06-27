1773d66385c51ddc29576269ce8c754d758141b1
Gavin Williams <gavin.williams@weareact.com>
2014-06-27 11:17:38 +0100

    Need to add karaf::params additions.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,32 +5,36 @@
 #
 class karaf::params {
   case $::osfamily {
-    'Debian' : { }
-    'RedHat' : { }
+    'RedHat' : { $karaf_add_path = true }
+    'Debian' : { $karaf_add_path = true }
     default  : { fail("${::osfamily} not supported") }
   }
 
   # Install method & location
   # Installation method. Can be: 'package','zip'.
-  $karaf_install_method    = 'zip'
-  $karaf_install_dir       = undef
+  $karaf_install_method = 'zip'
+  $karaf_install_dir    = undef
 
   # Default karaf temporary directory for downloading Zip.
-  $karaf_tmp_dir           = '/tmp'
+  $karaf_tmp_dir        = '/tmp'
 
   # Version
-  $karaf_version           = '3.0.1'
+  $karaf_version        = '3.0.1'
 
   # Should user be managed?
-  $karaf_manage_user       = true
-  $karaf_user              = 'karaf'
-  $karaf_group             = 'karaf'
+  $karaf_manage_user    = true
+  $karaf_user           = 'karaf'
+  $karaf_group          = 'karaf'
 
   # Default Karaf install parent directory.
-  $karaf_parent_dir        = '/opt'
+  $karaf_parent_dir     = '/opt'
 
   # Download location
-  $karaf_download_site = "http://www.apache.org/dyn/closer.cgi/karaf/${karaf_version}"
-  $karaf_download_file = "apache-karaf-${karaf_version}.tar.gz"
+  $karaf_download_site  = "http://mirror.catn.com/pub/apache/karaf/${karaf_version}"
+  $karaf_download_file  = "apache-karaf-${karaf_version}.tar.gz"
 
+  # Should a karaf service be created on installation?
+  # $karaf_create_service = true
+  # Default karaf service name
+  # $karaf_service_name   = undef
 }
