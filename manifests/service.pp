69774c21fb1df1837aa9a5dc20bcb3eaebf77719
Gavin Williams <gavin.williams@weareact.com>
2014-07-02 14:43:14 +0100

    Install the required karaf_feature to support karaf service installation.

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,10 +3,39 @@
 # This class is meant to be called from karaf
 # It creates and configures a karaf servicefile
 #
-class karaf::service  {
-  
-  exec { 'install_wrapper_feature':
-    command => "${karaf::karaf_dir}/bin/client feature:install wrapper"
+class karaf::service {
+  karaf_feature { 'wrapper': ensure => 'present' }
+
+  exec { 'install-service':
+    command => "client wrapper:install",
+    user    => $karaf::user,
+    path    => "${karaf::karaf_dir}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+    unless  => "test -f ${karaf::karaf_dir}/bin/karaf-service",
+    require => Karaf_feature['wrapper']
   }
-  
+
+  file { 'link-service':
+    ensure  => 'link',
+    target  => "${karaf::karaf_dir}/bin/karaf-service",
+    path    => '/etc/init.d/karaf-service',
+    require => Exec['install-service']
+  }
+
+  if $karaf::start_on_install {
+    exec { 'stop-karaf':
+      command => "sh -c \"${karaf::karaf_dir}/bin/stop\"",
+      user    => $karaf::user,
+      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+      onlyif  => 'ps -ef |grep org.apache.karaf.main.Main|grep -v grep',
+      require => File['link-service'],
+      before  => Service['karaf-service']
+    }
+  }
+
+  service { 'karaf-service':
+    ensure  => 'running',
+    enable  => true,
+    require => File['link-service']
+  }
+
 }
